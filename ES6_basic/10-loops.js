a8d39f3c8d74e331d53f6dc6d53bf17578e17056
whorvn <itzreone@gmail.com>
2024-07-21 18:15:34 +0400

    ES6

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+export default function appendToEachArrayValue(array, appendString) {
+  const new_array = array;
+  for (const value of array) {
+    const idx = array.indexOf(value);
+    new_array[idx] = appendString + value;
+  }
+  return new_array;
+}
