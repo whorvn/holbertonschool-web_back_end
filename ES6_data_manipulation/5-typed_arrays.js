1e35c81716b7a95969c25ea8ba19f7772dbc4f2b
itzreone <itzreone@gmail.com>
2024-07-25 23:11:59 +0400

    task 5

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+export default function createInt8TypedArray(length, position, value) {
+    if (position >= length || position <0 ){
+        throw new Error('Position out of range');
+    }
+    const buffer = new ArrayBuffer(length);
+    const veiw = new DataView(buffer);
+
+    veiw.setInt8(position, value);
+
+    return veiw;
+}
