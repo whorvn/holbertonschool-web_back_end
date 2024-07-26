3a04c8c9e9abbe492a973258f791a692e2618b94
itzreone <itzreone@gmail.com>
2024-07-26 13:37:10 +0400

    fix

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default function createInt8TypedArray(length, position, value) {
     if (position >= length || position <0 ){
-        throw new Error('Position outside of range');
+        throw new Error('Position outside range');
     }
     const buffer = new ArrayBuffer(length);
     const veiw = new DataView(buffer);
