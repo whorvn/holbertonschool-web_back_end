0eec4294eaf1e9d5eb6e5ffa17a6a312d3a34fd0
itzreone <itzreone@gmail.com>
2024-07-28 21:35:18 +0400

    commit

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,17 +1,33 @@
-/* eslint-disable */
-import {default as signUpUser} from './4-user-promise.js';
-import {default as uploadPhoto} from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-    const userData = await signUpUser(firstName, lastName).then((data) => ({
-        status: 'fulfilled',
-        value: data
-    }));
+async function handleProfileSignup(firstName, lastName, fileName) {
+  const promise1 = {
+    status: 'pending ',
+  };
+  const promise2 = {
+    status: 'pending ',
+  };
 
-    const photoData = await uploadPhoto(fileName).catch((err) => ({
-        status: 'rejected',
-        value: err.toString()
-    }));
+  try {
+    const signup = await signUpUser(firstName, lastName);
+    promise1.status = 'fulfilled';
+    promise1.value = signup;
+  } catch (err) {
+    promise1.status = 'rejected';
+    promise1.value = err.toString();
+  }
 
-    return Promise.resolve([userData, photoData]);
+  try {
+    const upload = await uploadPhoto(fileName);
+    promise2.status = 'fulfilled';
+    promise2.value = upload;
+  } catch (err) {
+    promise2.status = 'rejected';
+    promise2.value = err.toString();
+  }
+
+  return [promise1, promise2];
 }
+
+export default handleProfileSignup;
