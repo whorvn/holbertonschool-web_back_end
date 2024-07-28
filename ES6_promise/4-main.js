1a5e9827ac3434ba71339e97ee2ed928d7a513d7
itzreone <itzreone@gmail.com>
2024-07-28 21:55:05 +0400

    commit

---
+++ ES6_promise/4-main.js
@@ -0,0 +1,4 @@
+// 4-main.js
+import signUpUser from "./4-user-promise";
+
+console.log(signUpUser("Bob", "Dylan"));
--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,6 +1,10 @@
-export default function signUpUser(firstName, lastName) {
+// 4-user-promise.js
+
+function signUpUser(firstName, lastName) {
     return Promise.resolve({
-      firstName,
-      lastName,
+      firstName: firstName,
+      lastName: lastName
     });
-}
+  }
+  
+  export default signUpUser;
